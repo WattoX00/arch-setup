8663656d541fbd21698997d49e98ed17711ca367
wattox00 <korschzozinho@gmail.com>
2026-02-14 13:38:55 +0100

    eza and zoxide added

--- dwm-wattox.sh
+++ dwm-wattox.sh
@@ -15,7 +15,7 @@ setupDWM() {
   printf "%b\n" "${YELLOW}Installing DWM-Wattox...${RC}"
   case "$PACKAGER" in # Install pre-Requisites
   pacman)
-    "$ESCALATION_TOOL" "$PACKAGER" -S --needed --noconfirm base-devel libx11 libxinerama libxft imlib2 git unzip flameshot mate-polkit rofi alacritty xclip thunar tumbler gvfs thunar-archive-plugin dunst feh nwg-look xorg-xprop xorg-server xorg-xinit polybar picom xdg-user-dirs xdg-desktop-portal-gtk gnome-keyring flatpak networkmanager network-manager-applet brightnessctl python-pipx nvim noto-fonts-emoji noto-fonts-cjk ttf-meslo-nerd firefox fastfetch pipewire pipewire-alsa pipewire-pulseber alsa-utils pavucontrol nwg-look
+    "$ESCALATION_TOOL" "$PACKAGER" -S --needed --noconfirm base-devel libx11 libxinerama libxft imlib2 git unzip flameshot mate-polkit rofi alacritty xclip thunar tumbler gvfs thunar-archive-plugin dunst feh nwg-look xorg-xprop xorg-server xorg-xinit polybar picom xdg-user-dirs xdg-desktop-portal-gtk gnome-keyring flatpak networkmanager network-manager-applet brightnessctl python-pipx nvim noto-fonts-emoji noto-fonts-cjk ttf-meslo-nerd firefox fastfetch pipewire pipewire-alsa pipewire-pulseber alsa-utils pavucontrol nwg-look eza zoxide
     ;;
   *)
     printf "%b\n" "${RED}Unsupported package manager: ""$PACKAGER""${RC}"
