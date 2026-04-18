ca878acddcb23c5bfd483414104fb3da1d53e69d
Wattox <78638839+WattoX00@users.noreply.github.com>
2026-04-18 18:16:57 +0200

    Add lysn installation function to todol.sh
    Added installation function for lysn music player.

--- todol.sh
+++ todol.sh
@@ -1,7 +1,13 @@
 #!/bin/sh -e
 
-# todol is my python todo list app (i use it every day..)
+# todol is my python todo list app
 installTodol() {
   export PATH="$HOME/.local/bin:$PATH"
   pipx install todol
+}
+
+# lysn is my music player
+installLysn() {
+  export PATH="$HOME/.local/bin:$PATH"
+  pipx install lysn
 }
